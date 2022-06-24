75f3ba30939446b7b1d85c646b4290143a45c25c
Ashish <107981335+Codex-as@users.noreply.github.com>
2022-06-24 11:28:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function compute()
+{
+    let p=document.getElementById('amount').value;
+    let t=document.getElementById('time').value;
+    let r=document.getElementById('rate').value;
 
+    let  SI=(p*t*r)/100;
+
+    document.getElementById('si').innerHTML=" Total Simple Intrest Is: "+SI;
+}
